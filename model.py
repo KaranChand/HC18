3d008b850c7f8bcbf7ba523904393ccdf5969023
Freek <freek.hens@ru.nl>
2023-05-04 14:22:33 +0200

    custom loss added as option

--- model.py
+++ model.py
@@ -9,6 +9,120 @@ from keras.optimizers import *
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as keras
 
+# Custom loss function imports
+from sklearn.utils.extmath import cartesian
+import math
+
+
+def cdist(A, B):
+    """
+    Computes the pairwise Euclidean distance matrix between two tensorflow matrices A & B, similiar to scikit-learn cdist.
+
+    For example:
+
+    A = [[1, 2],
+         [3, 4]]
+
+    B = [[1, 2],
+         [3, 4]]
+
+    should return:
+
+        [[0, 2.82],
+         [2.82, 0]]
+    :param A: m_a x n matrix
+    :param B: m_b x n matrix
+    :return: euclidean distance matrix (m_a x m_b)
+    """
+    # squared norms of each row in A and B
+    na = tf.reduce_sum(tf.square(A), 1)
+    nb = tf.reduce_sum(tf.square(B), 1)
+
+    # na as a row and nb as a column vectors
+    na = tf.reshape(na, [-1, 1])
+    nb = tf.reshape(nb, [1, -1])
+
+    # return pairwise euclidead difference matrix
+    D = tf.sqrt(tf.maximum(na - 2 * tf.matmul(A, B, False, True) + nb, 0.0))
+    return D
+
+
+def weighted_hausdorff_distance(w, h, alpha):
+    all_img_locations = tf.convert_to_tensor(cartesian([np.arange(w), np.arange(h)]), dtype=tf.float32)
+    max_dist = math.sqrt(w ** 2 + h ** 2)
+
+    def hausdorff_loss(y_true, y_pred):
+        def loss(y_true, y_pred):
+            eps = 1e-6
+            y_true = keras.reshape(y_true, [w, h])
+            gt_points = keras.cast(tf.where(y_true > 0.5), dtype=tf.float32)
+            num_gt_points = tf.shape(gt_points)[0]
+            y_pred = keras.flatten(y_pred)
+            p = y_pred
+            p_replicated = tf.squeeze(keras.repeat(tf.expand_dims(p, axis=-1), num_gt_points))
+            d_matrix = cdist(all_img_locations, gt_points)
+            num_est_pts = tf.reduce_sum(p)
+            term_1 = (1 / (num_est_pts + eps)) * keras.sum(p * keras.min(d_matrix, 1))
+
+            d_div_p = keras.min((d_matrix + eps) / (p_replicated ** alpha + (eps / max_dist)), 0)
+            d_div_p = keras.clip(d_div_p, 0, max_dist)
+            term_2 = keras.mean(d_div_p, axis=0)
+
+            return term_1 + term_2
+
+        batched_losses = tf.map_fn(lambda x:
+                                   loss(x[0], x[1]),
+                                   (y_true, y_pred),
+                                   dtype=tf.float32)
+        return keras.mean(tf.stack(batched_losses))
+
+    return hausdorff_loss
+
+def keras_dice_coef(y_true, y_pred):
+  y_true_f = keras.flatten(y_true)
+  y_pred_f = keras.flatten(y_pred)
+  intersection = keras.sum(y_true_f * y_pred_f)
+  return (2. * intersection + 0.0001) / (keras.sum(y_true_f) + keras.sum(y_pred_f) + 0.0001)
+
+def get_metrics(y_img1, y_img2):
+    '''Function returns the four metrics used to compare two ellipse images
+    Input: 
+        image1: string, location of image1, e.g. training_set/023_HC_Annotation.png
+        image2: string, location of image2
+    Output:
+        abs_difference: float, metric 1. Pixel level absolute difference. Lower is more similar
+        dice_dissimilarity: float, metric 2. Dice dissimilarity. Lower is more similar
+        difference: float, metric 3. Pixel level difference. Lower is more similar
+        hausdorff_distance: float, metric 4. Hausdorff distance (longest distance between 2 sets of points). Lower is more similar.
+    '''
+    if keras.max(y_img1) > 1:
+        y_img1 = y_img1/255
+    if keras.max(y_img2) > 1:
+        y_img2 = y_img2/255
+
+    # Mean absolute difference (pixels): Lower is more similar
+    abs_difference = keras.sum(keras.abs(y_img1 - y_img2))
+
+    # Dice dissimilarity coefficient: Lower is more similar
+    dice = keras_dice_coef(keras.reshape(y_img1, [-1]), keras.reshape(y_img2, [-1]))
+
+    # Mean difference (pixels): Lower is more similar
+    difference = keras.sum(y_img1 - y_img2)
+
+    # sizex = y_img1.shape[0]
+
+    # # Mean Hausdorff distance (Lower is more similar)
+    # # Symmetric Hausdorff distance between two 2-D arrays of coordinates:
+    # hausdorff_distance = weighted_hausdorff_distance(sizex, sizex, 0)(np.expand_dims(y_img1, 0), np.expand_dims(y_img2, 0))
+
+    return abs_difference, dice, difference# , hausdorff_distance
+
+def custom_loss_function(y_true, y_pred):
+    '''Custom loss function consisting of a combination of 4 metrics'''
+    abs_diff, dice, diff = get_metrics(y_true, y_pred)
+    loss = 1/(abs_diff + dice + diff)
+    return tf.cast(loss, tf.float32)
+
 
 def unet(pretrained_weights = None,input_size = (256,256,1)):
     inputs = Input(input_size)
